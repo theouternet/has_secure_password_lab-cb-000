35bd71a16c74ff4bb2cf54465b0ef23b13c8dc0d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 21:56:35 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
-class SessionsController < ApplicationController 
-  
-    def create
-    @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
+class SessionsController < ApplicationController
+
+  def new
+
   end
-  
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to homepage_path
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to login_path
+  end
+
 end 
