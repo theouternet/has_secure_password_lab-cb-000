aa3ff6887a17fbc6028f22d2925bf487db4eae43
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 22:04:16 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   private
  
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
   
 end 
