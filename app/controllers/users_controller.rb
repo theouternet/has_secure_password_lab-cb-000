c08cb0ceea994e7230e62f2a607856c13daa23d6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 22:07:39 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
    @user = User.new(user_params)
    if @user.valid?
      @user.save
-     @user
+     session[:user_id] = @user.id
   end
  
   private
