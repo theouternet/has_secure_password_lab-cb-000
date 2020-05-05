8cb07911f0408748422f9eecc831a01afc404690
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 21:45:53 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resource :users, only: [:new, :create]
+
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  get '/homepage', to: 'users#homepage'
+  post '/logout', to: 'sessions#destroy'
 end
